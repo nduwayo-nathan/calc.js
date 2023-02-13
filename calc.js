e2ada9cc8a53c7dafd4707676339178e59ef5bfa
nduwayo-nathan <123937601+nduwayo-nathan@users.noreply.github.com>
2023-02-13 17:08:43 +0200

    Add files via upload
    my calculator of cat term 2

---
+++ calc.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const app = express();
+var fs = require("fs");
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/sum', function (req, res) {
+    var firstNumber = parseInt(req.body.a);
+   var secondNumber = parseInt(req.body.b);
+    var sum = Number(firstNumber + secondNumber);
+    res.send('The sum is: ' + Number(sum));
+
+})
+app.get('/product', function (req, res) {
+    var firstNumber = parseInt(req.body.a);
+   var secondNumber = parseInt(req.body.b);
+    var product = Number(firstNumber * secondNumber);
+    res.send('The product is: ' + Number(product));
+})
+app.listen(7000, function () {
+    console.log('App listening on port 7000!');
+  });
